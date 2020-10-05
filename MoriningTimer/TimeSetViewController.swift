0f41f72655de0482577a0d44b8f9b04ab8516338
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-10-05 15:06:59 +0900

    最新

--- MoriningTimer/TimeSetViewController.swift
+++ MoriningTimer/TimeSetViewController.swift
@@ -16,11 +16,12 @@ class TimeSetViewController: UIViewController,UITableViewDataSource, UITableView
     //タイマーの秒数を表示する
     var timeCountNumber: Int = 0
     var timer: Timer = Timer()
+    var changedtime = String()
     
     var orderarray = [String]()
     var contentarray = [String]()
     var timearray = [String]()
-//    let realm = try! Realm()
+    //    let realm = try! Realm()
     
     override func viewDidLoad() {
         table.register(UINib(nibName: "TimeSetCell", bundle: nil), forCellReuseIdentifier: "TimeSetCell")
@@ -33,22 +34,21 @@ class TimeSetViewController: UIViewController,UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TimeSetCell", for: indexPath) as! TimeSetCell
-        cell.orderLabel.text = "準備\(indexPath.row)"
-        print(cell.orderLabel.text)
-//        orderarray.append((cell?.orderLabel?.text)!)
-//        contentarray.append((cell?.contentTextField?.text)!)
-//        cell.timeLabel!.text = timearray[0]
+        let readynumber = indexPath.row + 1
+        cell.orderLabel.text = "準備\(readynumber)"
+        cell.timeLabel.text = changedtime
+        
+        //        orderarray.append((cell?.orderLabel?.text)!)
+        //        contentarray.append((cell?.contentTextField?.text)!)
+        
         return cell
         
     }
-    
-    
-
     
     func startTimer() {
         if !timer.isValid{
@@ -65,14 +65,15 @@ class TimeSetViewController: UIViewController,UITableViewDataSource, UITableView
         timeCountNumber = timeCountNumber + 1
         let second = timeCountNumber % 60
         let minute = (timeCountNumber - second) / 60
-       print(String(format: "%02d:%02d", minute,second))
+        changedtime = String(format: "%02d:%02d", minute,second)
+        self.table.reloadData()
+        print(changedtime)
     }
     
     @IBAction func start(){
         if timer.isValid{//一時停止
             timer.invalidate()
             startButton.setTitle("スタート", for: .highlighted)
-            
         }else{//タイマーを動かす
             startTimer()
             startButton.setTitle("ストップ", for: .highlighted)///ボタンが切り替わらない
@@ -86,21 +87,22 @@ class TimeSetViewController: UIViewController,UITableViewDataSource, UITableView
     
     @IBAction func next(){
         //このボタンを押すごとに要素を配列に追加してそれをtableviewで表示
-
- 
+        //next押したら配列に追加してlabelには配列のindexpath.rowばん目を表示させる。一つ前のせるのlabelはもうそれで固定する= 最新のセルだけ更新するとか？
+        timearray.append(changedtime)
+        
     }
     
     @IBAction func save(){
-//        let updatetimersetdata = TimerSetData()
-//        updatetimersetdata.readtnumber =
-//            updatetimersetdata.content = //"ここでtextfieldに入力した内容を取得する"
-//            updatetimersetdata.time = timeCountNumber
+        //        let updatetimersetdata = TimerSetData()
+        //        updatetimersetdata.readtnumber =
+        //            updatetimersetdata.content = //"ここでtextfieldに入力した内容を取得する"
+        //            updatetimersetdata.time = timeCountNumber
         
-//        if timeCountNumber != 0{
-//            try! realm.write(){
-//                realm.add(updatetimersetdata)
-//                // let timerData = try! Realm().objects(TimerSetData.self)
-//            }
-//        }
- }
+        //        if timeCountNumber != 0{
+        //            try! realm.write(){
+        //                realm.add(updatetimersetdata)
+        //                // let timerData = try! Realm().objects(TimerSetData.self)
+        //            }
+        //        }
+    }
 }
