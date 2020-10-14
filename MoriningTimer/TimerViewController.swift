4e840ffe8c84c50381bfe56b0d404b116ed8629c
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-10-14 23:17:13 +0900

    listに保存が出来ちゃったけど、リスト使わない方法に変えようと思うのでここでpush

--- MoriningTimer/TimerViewController.swift
+++ MoriningTimer/TimerViewController.swift
@@ -16,14 +16,25 @@ class TimerViewController: UIViewController,UITableViewDataSource, UITableViewDe
     @IBOutlet var tillArriveLabel: UILabel!
     
     let realm = try! Realm()
+
     var timer:Timer = Timer()
     var timeCountNumber: Int = 0
+//    var orderarray: [String] = []
+    var todoarray: [String] = []
+    var timearray = [Int]()  //時間ように変更したのじゃないのを保存するためにint型にしてみた
+    
+    let timerSetDataArray = realm.objects(TimerSetData.self)
+    let orderList = List<String>() //List型
+    var orderarray = Array<Any>() // Array型
+//    let orderarray.append(contentsOf: Array(orderList)) // Array()でListを変換
+    let orderarray = Array<Any>()
 
     override func viewDidLoad() {
         table.register(UINib(nibName: "TimerCell", bundle: nil), forCellReuseIdentifier: "TimerCell")
         super.viewDidLoad()
         table.dataSource = self
         table.delegate = self
+        timersetdata = realm.objects(TimerSetData.self)
         
         // print(Realm.Configuration.defaultConfiguration.fileURL)
     }
